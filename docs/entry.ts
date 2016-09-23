68ec2fa396fb819ba4d1407783bc2ed6d741455a
bokuweb <bokuweb12@gmail.com>
2016-09-23 21:52:51 +0900

    Update dependencies

--- docs/entry.ts
+++ docs/entry.ts
@@ -1,8 +1,8 @@
 /// <reference path="../node_modules/@types/es6-shim/index.d.ts" />
 
+import 'zone.js/dist/zone';
 import 'es6-shim';
 import 'reflect-metadata';
-import 'zone.js/dist/zone';
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { ExampleModule } from './example.module';
